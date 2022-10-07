6532f058987ef3e4a03fec4eb547c07410de8f69
ligf <ligf731@gmail.com>
2022-10-07 11:50:47 +0800

    Add ReuseBuffer Class

---
+++ ElegantSeries.SerialPort/Utilities/ReuseBuffer.cs
@@ -0,0 +1,30 @@
+﻿namespace ElegantSeries.Utilities
+{
+    public class ReuseBuffer
+    {
+        int index = 0;
+        object? _Data;
+
+        public int Number { get; set; } = 0;
+        public object? Data
+        {
+            get
+            {
+                if (index == Number)
+                {
+                    _Data = null;
+                }
+                else
+                {
+                    index++;
+                }
+                return _Data;
+            }
+            set
+            {
+                _Data = value;
+                index = 0;
+            }
+        }
+    }
+}
